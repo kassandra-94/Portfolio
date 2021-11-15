4e032100b07bd831b608a5b022cf51f41f04673e
sara <chaire.kassandra@gmail.com>
2021-11-15 19:37:56 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  extends: [],
+  purge:  [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors:{
+      green:{
+        light: "#d7fdce",
+        default: "#5ef939",
+        dark: "#166603",
+      },
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.sky,
+      yellow: colors.amber,
+      lime: colors.lime,
+      teal: colors.teal,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
