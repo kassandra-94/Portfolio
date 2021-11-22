bbdad350d745e71b7e51e0e214496d1df4fd3bb5
sara <chaire.kassandra@gmail.com>
2021-11-22 15:08:35 +0100

    purge function

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   extends: [],
-  purge:  [],
+  purge: { 
+    enabled: true,
+    content:[ './src/**/*.html'],
+},
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors:{
