09fe3a4dab6b2f9e8628850e4a9dc36ee61acc60
sara <chaire.kassandra@gmail.com>
2021-12-11 00:08:26 +0100

    closing button
    style.-display -block

--- menu.js
+++ menu.js
@@ -1,11 +1,22 @@
-let button = document.querySelector('button');
+let button = document.getElementById('button');
 let menu =  document.getElementById("menu_dropdown");
-
+let closingButton = document.getElementById("close");
 
       function hideAndShow() {
-        if (menu.style.display === "none") {
+        if (menu.style.display === "none" && closingButton.style.display === "none") {
           menu.style.display = "block";
+          button.style.display= "none";
+          closingButton.style.display ="block";
         } else {
           menu.style.display = "none";
+          closingButton.style.display ="none";
+        }
+      };
+
+      function backToMenu() {
+        if (menu.style.display === "block" && closingButton.style.display ==="block") {
+          menu.style.display = "none";
+          closingButton.style.display ="none";
+          button.style.display= "block";
         }
       };
