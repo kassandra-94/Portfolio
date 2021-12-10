74acbb8d62d5b2617dea46f4d69cfe8aef835489
sara <chaire.kassandra@gmail.com>
2021-12-10 19:12:21 +0100

    menu responsive
    introduction

---
+++ menu.js
@@ -0,0 +1,11 @@
+let button = document.querySelector('button');
+let menu =  document.getElementById("menu_dropdown");
+
+
+      function hideAndShow() {
+        if (menu.style.display === "none") {
+          menu.style.display = "block";
+        } else {
+          menu.style.display = "none";
+        }
+      };
